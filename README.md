2f031e234abc821fe1bc8b3c05643aa6b6d4ca1e
Sanyog Yadav <sanesanyo@gmail.com>
2022-07-07 22:28:20 +0200

    React app to deploy the EpicNFTs smart contract

--- README.md
+++ README.md
@@ -1,11 +1,23 @@
 # buildspace Mint Your Own NFT Collection
 
 ### **Welcome 👋**
-To get started with this course, clone this repo and follow these commands:
 
 1. Run `npm install` at the root of your directory
-2. Run `npm run start` to start the project
-3. Start coding!
+2. Run `npm run start` to start the project locally
+3. Run `npm run build` to create build file for the project
+4. In order to deploy it on Google Cloud run `gcloud app deploy` (before you deploy please make sure to check the instructions below)
 
-### **Questions?**
-Have some questions make sure you head over to your [buildspace Dashboard](https://app.buildspace.so/courses/CO961ddb5f-f428-4608-9949-a9a2f461eb3f) and link your Discord account so you can get access to helpful channels and your instructor!
+
+Remember when you deployed your contract to the Rinkeby Testnet (epic btw)? The output from that deployment included your smart contract address which should look something like this:
+
+Deploying contracts with the account: 0xF79A3bb8d5b93686c4068E2A97eAeC5fE4843E7D
+Account balance: 3198297774605223721
+WavePortal address: 0xd5f08a0ae197482FA808cE84E00E97d940dBD26E
+
+Add the address like you see for Waveportal address above i.e. 0xd5f08a0ae197482FA808cE84E00E97d940dBD26E to line 87 in src\App.js i.e. const CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS HERE";
+
+Also make sure to your own ABI file for your smart contract in \src\utils. The contents of the ABI file can be found in a fancy JSON file in your hardhat project, something similar to shown below:
+
+artifacts/contracts/WavePortal.sol/WavePortal.json
+
+
--- src/App.js
+++ src/App.js
@@ -1,20 +1,137 @@
-import './styles/App.css';
-import twitterLogo from './assets/twitter-logo.svg';
-import React from "react";
+import "./styles/App.css";
+import twitterLogo from "./assets/twitter-logo.svg";
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import myEpicNFT from "./utils/MyEpicNFT.json";
+import convertToNFT from "./utils/ConvertToNFT.json";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = '';
+const OPENSEA_LINK = "";
 const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
-  // Render Methods
-  const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
-      Connect to Wallet
-    </button>
-  );
+  const [currentAccount, setCurrentAccount] = useState();
+  const [wallet, setWallet] = useState(true);
+  const [accountConnected, setAccountConnected] = useState(false);
+  const [nftUrls, setNftUrls] = useState([]);
+  const [mintNft, setMintNft] = useState(false);
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      setWallet(false);
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+      setAccountConnected(ethereum._state.isConnected);
+    }
+
+    // Check if we are authorised to access the user wallet
+
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  };
+
+  /*
+   * Implement your connectWallet method here
+   */
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.alert("Get Metamask!");
+        window.open("https://metamask.io/download/", "_blank").focus();
+        return;
+      }
+
+      let chainId = await ethereum.request({ method: "eth_chainId" });
+      console.log("Connected to chain " + chainId);
+
+      // String, hex code of the chainId of the Rinkebey test network
+      const rinkebyChainId = "0x4";
+      if (chainId !== rinkebyChainId) {
+        alert("You are not connected to the Rinkeby Test Network!");
+      }
+
+      // Check if we are authorised to access the user wallet
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts"
+      });
+
+      /*
+       * Boom! This should print out public address once we authorize Metamask.
+       */
+      console.log("Connected", accounts[0], ethereum._state);
+      setAccountConnected(ethereum._state.isConnected);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const askContractToMintNft = async () => {
+    const CONTRACT_ADDRESS = "0x2CD75a9E8f1cCc096cE24F08bdAe0DeAE443134c";
+
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          convertToNFT.abi,
+          signer
+        );
+
+        console.log("Going to pop wallet now to pay gas");
+        let nftTxn = await connectedContract.makeAnEpicNFT(
+          "https://cloudflare-ipfs.com/ipfs/Qmbc9V13DR8d58xXnCT2oW2t2eHUk6imr1xk6yQACeWwoM",
+          "My Signature",
+          "First NFT"
+        );
+
+        console.log("Mining...please wait.");
+        setMintNft(true);
+        await nftTxn.wait();
+
+        console.log(
+          `Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`
+        );
+
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber());
+          console.log(
+            `Hey there! We've minted your NFT. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: <https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}>`
+          );
+          setNftUrls([
+            ...nftUrls,
+            `https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          ]);
+        });
+      } else {
+        console.log("Ethereum object does not exist");
+      }
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
 
   return (
     <div className="App">
@@ -24,7 +141,49 @@ const App = () => {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {renderNotConnectedContainer()}
+          {!accountConnected ? (
+            wallet ? (
+              <button
+                className="cta-button connect-wallet-button"
+                onClick={() => connectWallet()}
+              >
+                Connect to Wallet
+              </button>
+            ) : (
+              <button
+                className="cta-button connect-wallet-button"
+                onClick={() =>
+                  window.open("https://metamask.io/download/", "_blank").focus()
+                }
+              >
+                Download Metamask
+              </button>
+            )
+          ) : (
+            <button
+              className="cta-button connect-wallet-button"
+              onClick={() => askContractToMintNft()}
+            >
+              {mintNft ? "Minting NFTs" : "Mint NFTs"}
+            </button>
+          )}
+          <div>
+            {nftUrls.length !== 0
+              ? nftUrls.map((url, i) => (
+                  <button
+                    className="cta-button connect-wallet-button"
+                    key={i}
+                    onClick={() => window.open(url, "_blank")}
+                  >
+                    {`NFT ${i + 1}`}
+                  </button>
+                ))
+              : mintNft && (
+                  <button className="cta-button connect-wallet-button">
+                    Mining NFTs...Please Wait
+                  </button>
+                )}
+          </div>
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
